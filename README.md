350cf214ae801841283383790b3e98a6b238e7ee
Fabio Ribeiro <faabiosr@gmail.com>
2021-11-01 12:05:14 +0100

    Link files in Windows
    Link powershell script to setup in Windows.

--- README.md
+++ README.md
@@ -3,12 +3,25 @@
 ## Installation
 Clone this repository and run the link script
 
-Link files
+### Link files
+
+**Unix**
 ```sh
 $ sh exegol/link.sh
 ```
 
-Unlink files
+**Windows**
+```
+exegol\link.ps1
+```
+
+### Unlink files
+**Unix**
 ```sh
 $ sh exegol/link.sh -u
+```
+
+**Windows**
+```
+exegol\link.ps1 -undo
 ```
---
+++ link.ps1
@@ -0,0 +1,39 @@
+<#
+.SYNOPSIS
+  Link script
+.DESCRIPTION
+  Part of https://github.com/faabiosr/exegol
+.NOTES
+  Author: Fabio Ribeiro - faabiosr@gmail.com
+#>
+
+param(
+    [switch]$undo
+)
+
+$files = @(
+    '.config\nvim\init.vim',
+    'AppData\Local\Microsoft\Windows Terminal\settings.json'
+)
+
+$script ="$PSScriptRoot"
+$homeDir = "$script\home"
+
+if ($undo) {
+    Write-Host "unlinking files"
+    foreach ($file in $files) {
+        Write-Host "=> $file"
+        if (Test-Path "$HOME\$file") {
+            Remove-Item -Path "$HOME\$file"
+        }
+    }
+
+    return
+}
+
+Write-Host "linking files"
+foreach ($file in $files) {
+    Write-Host "=> $file"
+    New-Item -ItemType Directory -Path (Split-Path -Path "$HOME\$file") -Force | Out-Null
+    New-Item -ItemType SymbolicLink -Path "$HOME\$file" -Target "$homeDir\$file" -Force | Out-Null
+}
